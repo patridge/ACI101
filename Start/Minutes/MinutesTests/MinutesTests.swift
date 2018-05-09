b81c61266ec24e444e1ac53ce5ce815b3f6e48bc
Adam Patridge <patridge@gmail.com>
2018-05-09 08:32:29 -0600

    Add first test.

---
+++ Start/Minutes/MinutesTests/MinutesTests.swift
@@ -0,0 +1,32 @@
+//
+//  MinutesTests.swift
+//  MinutesTests
+//
+//  Created by Adam Patridge on 5/9/18.
+//  Copyright © 2018 Microsoft. All rights reserved.
+//
+
+import XCTest
+@testable import Minutes
+
+class MinutesTests: XCTestCase {
+    var entryUnderTest: Entry!
+    
+    override func setUp() {
+        super.setUp()
+        entryUnderTest = Entry("Title", "Content")
+    }
+    
+    override func tearDown() {
+        entryUnderTest = nil
+        super.tearDown()
+    }
+    
+    func testSerialization() {
+        let serializedData = FileEntryStore.serialize(entryUnderTest)
+        let entry = FileEntryStore.deserialize(serializedData!)
+        
+        XCTAssertEqual(entry?.title, "Title", "Title does not match")
+        XCTAssertEqual(entry?.content, "Content", "Content does not match")
+    }
+}
